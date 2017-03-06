2bf23757cc1b808bdc66d7ec530e75dd68129705
Hugo Talbot <hugotalbot@rved.home>
2017-03-06 23:08:31 +0100

    Cosmetic reindent and added JS into subscribe.tpl

--- Acuponcture/scripts/subscription.js
+++ Acuponcture/scripts/subscription.js
@@ -1,18 +1,24 @@
 	
 function checkConfirmEmail(input) {
-	  if (input.value != document.getElementById('email_addr').value) {
+	if (input.value != document.getElementById('email_addr').value) {
 		input.setCustomValidity('Mail incorrecte');
-	  } else {
+	} else {
 		// le champ est valide : on réinitialise le message d'erreur
 		input.setCustomValidity('');
-	  }
+	}
 }
 
 function checkConfirmPassword(input) {
-	  if (input.value != document.getElementById('password').value) {
+	if (input.value != document.getElementById('password').value) {
 		input.setCustomValidity('Password incorrecte');
-	  } else {
+	} else {
 		// le champ est valide : on réinitialise le message d'erreur
 		input.setCustomValidity('');
-	  }
+	}
+}
+
+function createAccount(form){
+
+	//creer le compte dans la bdd
+
 }
